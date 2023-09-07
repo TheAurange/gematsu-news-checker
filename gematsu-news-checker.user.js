fc22e2795e16cf4235c2d3fe38c3d6677843e2f2
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-09-06 19:35:27 -0500

    Update gematsu-news-checker.user.js

--- gematsu-news-checker.user.js
+++ gematsu-news-checker.user.js
@@ -1,15 +1,12 @@
 // ==UserScript==
-// @name    [Gematsu] News Checker
+// @name    [Gematsu] News Tracker
 // @author  Aurange
-// @version 1.0
+// @version 1.1
 // @match   https://www.gematsu.com/
 // @grant   window.close
 // ==/UserScript==
 
 "use strict";
 
-if(localStorage.getItem("latest") === null) localStorage.setItem("latest", document.querySelectorAll(".gematsu-post__info > h2 > a")[5].innerText);
-else if(document.querySelectorAll(".gematsu-post__info > h2 > a")[5].innerText === localStorage.getItem("latest")){
-  window.close();
-}
-else localStorage.setItem("latest", document.querySelectorAll(".gematsu-post__info > h2 > a")[5].innerText);
+if(localStorage.getItem("latest") === null || document.querySelectorAll(".gematsu-post__info > h2 > a")[5].innerText !== localStorage.getItem("latest")) localStorage.setItem("latest", document.querySelectorAll(".gematsu-post__info > h2 > a")[5].innerText);
+else window.close();
